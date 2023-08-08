3f2038036229fa3f9166c1fde3e1f53d28710462
sergey-komar <kursev81@yandex.ru>
2023-08-08 19:41:15 +0300

    mobile

--- app/js/main.js
+++ app/js/main.js
@@ -175,6 +175,10 @@ $(function () {
     $('.questions-accardion__btn').on('click', function(){
         $(this).next().slideToggle(500); 
      });
+     $('.stock-accardion__btn').on('click', function(){
+      $(this).next().slideToggle(500); 
+   });
+    
 })
 
 
@@ -184,7 +188,6 @@ const tabsItem = document.querySelector('.about-tabs__item'),
         tabsBtn = document.querySelectorAll('.about-tabs__item-btn'),
        tabsContent = document.querySelectorAll('.about-tabs__content');
        
-
 function tabsHide() {
     tabsContent.forEach(item => {
         item.classList.add('hide');
@@ -225,16 +228,86 @@ if(tabsItem && tabsBtn && tabsContent){
 
 
 
- const menu = document.querySelector('.test');
- const mobile = document.querySelector('.nav-icon');
- const mobBtn = document.querySelector('.mobile');
+//  const menu = document.querySelector('.test');
+//  const mobile = document.querySelector('.nav-icon');
+//  const mobBtn = document.querySelector('.mobile');
 
- mobile.addEventListener('click', function(){
-     this.classList.toggle('nav-icon--active');
-     menu.classList.toggle('nav--active');
-     mobBtn.classList.toggle('visota')
- });
+//  mobile.addEventListener('click', function(){
+//      this.classList.toggle('nav-icon--active');
+//      menu.classList.toggle('nav--active');
+//      mobBtn.classList.toggle('visota')
+//  });
 
 
+
+ const tabsItemPersonal = document.querySelector('.personal-tabs__item'),
+        tabsBtnPersonal = document.querySelectorAll('.personal-tabs__item-btn'),
+       tabsContentPersonal = document.querySelectorAll('.personal-tabs__content');
+       
+function tabsHidePersonal() {
+    tabsContentPersonal.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show');
+    });
+    tabsBtnPersonal.forEach(btn => {
+        btn.classList.remove('personal-tabs--active')
+    });
+}
+
+function tabsShowPersonal(i) {
+    tabsContentPersonal[i].classList.add('show');
+    tabsContentPersonal[i].classList.remove('hide');
+    tabsBtnPersonal[i].classList.add('personal-tabs--active');
+}
+
+
+if(tabsItemPersonal){
+  tabsItemPersonal.addEventListener('click', (e) => {
+    const target = e.target;
+        if(target && target.classList.contains('personal-tabs__item-btn')){
+            tabsBtnPersonal.forEach((item, i) => {
+                if(target == item){
+                    tabsHidePersonal();
+                    tabsShowPersonal(i);
+                }
+            })
+        }
+});
+}
+
+
+if(tabsItemPersonal && tabsBtnPersonal && tabsContentPersonal){
+  tabsHidePersonal();
+  tabsShowPersonal(0);
+}
+
+//modal entry
+const modal = document.querySelector('.entry-modal'),
+      modalClose = document.querySelector('.entry-form__close'),
+      modalBtn = document.querySelector('.entry__btn');
+
+modalBtn.addEventListener('click', () => {
+  modal.classList.add('entry-form__show');
+  modal.classList.remove('entry-form__hide');
+})
+modalClose.addEventListener('click', () => {
+  modal.classList.add('entry-form__hide');
+  modal.classList.remove('entry-form__show');
+})
+
+
+//modal entry
+const modalGarage = document.querySelector('.garage'),
+      modalCloseGarage = document.querySelector('.garage__block__close'),
+      modalBtnGarage = document.querySelector('.garage__button');
+
+modalBtnGarage.addEventListener('click', () => {
+  modalGarage.classList.add('entry-form__show');
+  modalGarage.classList.remove('entry-form__hide');
+})
+modalCloseGarage.addEventListener('click', () => {
+  modalGarage.classList.add('entry-form__hide');
+  modalGarage.classList.remove('entry-form__show');
+})
 
 });
